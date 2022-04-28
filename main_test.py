ffb9e23aaf7c1777b83a2050be9d021a2c9ed9f2
Truxton-Marley <stevenstruxtonm@gmail.com>
2022-04-28 21:11:37 +0200

    Add test version of main for fib questions

---
+++ main_test.py
@@ -0,0 +1,9 @@
+import random
+
+from quiz_tools import ask_questions
+from quiz_tools import clear_screen_slowly
+from quiz_tools import get_fib_question
+
+file = "./fill_in_the_blanks/mpls_static_lsp.txt"
+
+get_fib_question(file)
